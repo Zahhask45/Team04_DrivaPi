7446b3a8bbf0b12feb32ba534708c4a6cdf510b9
Hugo Lopes <hugofslopes@live.com.pt>
2025-11-20 16:06:06 +0000

    pega la chefe

---
+++ qt_app/myqtapp-build/CMakeFiles/myqtapp.dir/myqtapp_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/myqtapp.dir/myqtapp_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
+ /home/drivapi/qt_app/myqtapp-build/myqtapp_autogen/3YJK5W5UP7/qrc_resources.cpp \
+ /usr/include/stdc-predef.h
