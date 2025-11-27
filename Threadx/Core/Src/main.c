cbdf5bdca6e0df4e5dacae88fbb22a332148acf3
Bernardo Esteves <119340710+berestv@users.noreply.github.com>
2025-11-27 17:37:30 +0000

    Fixed some minor bugs in the code implementation

--- Threadx/Core/Src/main.c
+++ Threadx/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "speed_sensor.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
--- Threadx/Debug/Core/Src/main.d
+++ Threadx/Debug/Core/Src/main.d
@@ -34,7 +34,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/app_threadx.h \
  ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_uart_ex.h \
  ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_exti.h \
  ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_fdcan.h \
- ../Core/Inc/main.h
+ ../Core/Inc/main.h ../Core/Inc/speed_sensor.h
 ../Core/Inc/app_threadx.h:
 ../Middlewares/ST/threadx/common/inc/tx_api.h:
 ../Middlewares/ST/threadx/ports/cortex_m33/gnu/inc/tx_port.h:
@@ -73,3 +73,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/app_threadx.h \
 ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_exti.h:
 ../Drivers/STM32U5xx_HAL_Driver/Inc/stm32u5xx_hal_fdcan.h:
 ../Core/Inc/main.h:
+../Core/Inc/speed_sensor.h:
