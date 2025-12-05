2a8e2a803fb4d82e1c61902278f863f69ceb3ab5
Hugofslopes <hugo.lopes@seame.pt>
2025-12-05 15:17:57 +0000

    Refactor CAN message transmission and formatting

--- Threadx/Core/Src/canTX.c
+++ Threadx/Core/Src/canTX.c
@@ -2,21 +2,10 @@
 int can_send(t_can_message* msg);
 
 void FloatToUint8(uint8_t* buffer, float number, int precision) {
-    // 1. Handle negative numbers
     float val = (number < 0) ? -number : number;
-
-    // 2. Extract the integer part
     int intPart = (int)val;
-
-    // 3. Extract the decimal part
-    // We multiply by 10^precision to turn decimals into an integer
-    // e.g., 0.456 * 100 = 45.6 -> (int)45
     float remainder = val - intPart;
     int decPart = (int)(remainder * pow(10, precision));
-
-    // 4. Format into the buffer
-    // %c = sign, %d = integer, . = dot, %0*d = decimal part with leading zeros
-    // The "0*d" ensures that 3.05 prints as "3.05" not "3.5"
     sprintf((char*)buffer, "%d.%0*d", intPart, precision, decPart);
 }
 
@@ -25,26 +14,9 @@ void make_speed_status_msg(t_can_message *msg, float speed)
     msg->id = 0x100;
     msg->len = 4;
 
-    // Copy float bytes to CAN data
-
-    // --- DEBUG PRINTING ---
-    // uint8_t TxData[8];
-
-    // FloatToUint8(TxData, speed, 2);
-
-    // 1. Extract the float back from the data bytes (safe against alignment issues)
-    //memcpy(TxData, &speed, 4);
-
-    // 2. Format to string (%.2f limits to 2 decimal places)
-
-    // 3. Transmit
-
     memcpy(msg->data, &speed, 4);
-
     HAL_UART_Transmit(&huart1, (uint8_t*)msg->data, 4, 100);
-
     can_send(msg);
-
 }
 
 int can_send(t_can_message* msg)
@@ -60,8 +32,8 @@ int can_send(t_can_message* msg)
     TxHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
     TxHeader.MessageMarker = 0;
 
-    char err_msg[20] = "TransCan!\r\n";
-    HAL_UART_Transmit(&huart1, (uint8_t*)err_msg, strlen(err_msg), 10);
+    char msg[20] = "TransCan!\r\n";
+    HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), 10);
 
     if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, (uint8_t*)msg->data) != HAL_OK)
     {
