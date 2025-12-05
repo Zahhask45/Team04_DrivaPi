2e17925a37b18ced610048e5d64c757d0549fd2c
Melanie Reis <your-username@users.noreply.github.com>
2025-12-05 08:47:56 +0000

    Remove deprecated configuration and deployment scripts for Kuksa Databroker

--- scripts/deploy/kuksa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -d --name kuksa-databroker \
-  -p 55555:55555 \
-  -v $(pwd)/config/vss/drivapi.vss.json:/etc/kuksa/drivapi.vss.json \
-  ghcr.io/eclipse-kuksa/kuksa-databroker:master \
-  --metadata /etc/kuksa/drivapi.vss.json
