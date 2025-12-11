54fa7630e1f969c086675723a4f1a147a22ce302
melaniereis <166030520+melaniereis@users.noreply.github.com>
2025-12-11 14:06:00 +0000

    Add gRPC includes and namespace for KUKSAReader class

--- qt_app/include/kuksareader.hpp
+++ qt_app/include/kuksareader.hpp
@@ -2,6 +2,11 @@
 #define KUKSAREADER_HPP
 
 #include <QObject>
+#include <grpcpp/grpcpp.h>
+#include "val.grpc.pb.h"
+
+// KUKSA Val gRPC namespace to avoid long names
+using namespace kuksa::val::v1;
 
 class KUKSAReader : public QObject
 {
