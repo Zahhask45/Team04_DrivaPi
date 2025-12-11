5b7b11be2d5ec80065047911ec3c661f1c92252c
melaniereis <166030520+melaniereis@users.noreply.github.com>
2025-12-11 14:10:29 +0000

    Refactor KUKSAReader class to add start slot and speedReceived signal

--- qt_app/include/kuksareader.hpp
+++ qt_app/include/kuksareader.hpp
@@ -14,8 +14,17 @@ class KUKSAReader : public QObject
 public:
     explicit KUKSAReader(QObject *parent = nullptr);
     ~KUKSAReader() override;
+public slots:
+    // Starts the data fetching process
+    void start();
+
+signals:
+    // Emitted when new speed data is received
+    void speedReceived(float speed);
 
 private:
+    // gRPC client stub -> used to communicate with KUKSA Val server
+    std::unique_ptr<VAL::Stub> m_stub_;
 };
 
 #endif // KUKSAREADER_HPP
