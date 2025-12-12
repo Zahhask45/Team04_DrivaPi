9c6772e8dfb7a3ef19a3fcac6fd61cdf4ce4c579
melaniereis <166030520+melaniereis@users.noreply.github.com>
2025-12-12 16:26:23 +0000

    Add function to parse Sender log lines in latency analyzer

--- docs/data_transfer/KUKSA/latency/latency_analyzer.cpp
+++ docs/data_transfer/KUKSA/latency/latency_analyzer.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iomanip>
 
 // Define a structure to hold latency sample data
 struct Sample
@@ -7,6 +12,21 @@ struct Sample
     double t0; // timestamp when sent
     double t1; // timestamp when received
     bool received;
+}
+
+// Auxiliary function to parse line from Sender log
+Sample parseSenderLogLine(const std::string &line, float &speed, double& timestamp)
+{
+    std::istringstream iss(line);
+    std::string temp;
+    
+    iss >> temp >> temp; // Skip "Sender:" and "Sent"
+    if (iss.fail()) return false;
+    iss >> speed;
+    iss >> temp >> temp; // Skip "at" "timestamp:"
+
+    iss >> timestamp;
+    return (!iss.fail());
 }
 
 int main()
